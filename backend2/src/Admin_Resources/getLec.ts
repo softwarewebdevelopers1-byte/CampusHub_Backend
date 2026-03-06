e6afa86a109a2a81c2e944732f3d6dd6b23e86ff
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-03-06 14:20:30 +0300

    Adding get lecturer route

---
+++ backend2/src/Admin_Resources/getLec.ts
@@ -0,0 +1,35 @@
+import { LecturerAcc as User } from "#models/user.model";
+import { authenticateAdmin } from "#Verification/access.token";
+import { Router } from "express";
+import type { Response, Request } from "express";
+
+export let GetLecturers = Router();
+export let UserNumber = Router();
+GetLecturers.get(
+  "/",
+  authenticateAdmin,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      let users: any[] = await User.find({}    , {
+        email: true,
+        role: true,
+        status: true,
+        account_state: true,
+        _id: false,
+      })
+        .lean()
+        .exec();
+      res.status(200).json({ Users: users });
+    } catch (error) {
+      res.status(500).json({ error: "Fetching users error" });
+    }
+  },
+);
+UserNumber.get("/", async (req: Request, res: Response): Promise<void> => {
+  try {
+    let count = await User.countDocuments({}    );
+    res.status(200).json({ count });
+  } catch (error) {
+    res.status(500).json({ error: "Counting users error" });
+  }
+});
--- backend2/src/server.ts
+++ backend2/src/server.ts
@@ -32,6 +32,7 @@ import { usersGetOwnPDF } from "#PdfResources/users.getPDF";
 import { simpleSearchRoute } from "#PdfResources/simple.search";
 import { LecturerCreateAccount } from "./lecturer_resources/Lecture_create_Account.js";
 import { LecturerLoginRoute } from "./lecturer_resources/lecturer_login_Account.js";
+import { GetLecturers } from "#adminResources/getLec";
 // import { globaLimit } from "#Verification/rate.limit";
 dotenv.config();
 let PORT = Number(process.env.DEV_PORT) || Number(process.env.PORT);
@@ -72,6 +73,8 @@ App.use("/auth/check/admin/logged", IsAdminLogged);
 App.use("/auth/all/admin/logout/all", AdminLogOutAll);
 App.use("/auth/verify/admin", LogAdminRouter);
 // lecturer routes
+// lecturer accounts
+App.use("/auth/lecturer/get/accounts",GetLecturers)
 // lecturer login route
 App.use("/auth/lecturer/login/account",LecturerLoginRoute);
 // lecturer create account route
